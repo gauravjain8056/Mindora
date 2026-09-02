c7c3e7373bf7e1d809f9d85df226e527d2d8eb1f
Gaurav Jain <gauravjainlumia7417@gmail.com>
2026-09-02 23:35:21 +0530

    gateway fix

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -8,25 +8,25 @@ import { getCurrentUser } from "./controllers/user.controller.js"
 import protect from "./middleware/auth.middleware.js"
 import { proxyWithHeader } from "./utils/proxyWithHeader.js"
 import morgan from "morgan"
-const port =process.env.PORT||8000
+const port = process.env.PORT
 
-const app=express()
+const app = express()
 app.use(cors({
-    origin:process.env.FRONTEND_URL,
-    credentials:true
+    origin: process.env.FRONTEND_URL,
+    credentials: true
 }))
 app.use(morgan("dev"))
 app.use(cookieParser())
-app.use("/api/auth",proxy(process.env.AUTH_SERVICE, { timeout: 120000 }))
-app.use("/api/chat",protect,proxyWithHeader(process.env.CHAT_SERVICE))
-app.use("/api/agent",protect,proxyWithHeader(process.env.AGENT_SERVICE))
-app.use("/api/billing",protect,proxyWithHeader(process.env.BILLING_SERVICE))
-app.get("/api/me",protect,getCurrentUser)
-app.get("/",(req,res)=>{
-    res.json({message:"hello from gateway v5"})
+app.use("/api/auth", proxy(process.env.AUTH_SERVICE, { timeout: 120000 }))
+app.use("/api/chat", protect, proxyWithHeader(process.env.CHAT_SERVICE))
+app.use("/api/agent", protect, proxyWithHeader(process.env.AGENT_SERVICE))
+app.use("/api/billing", protect, proxyWithHeader(process.env.BILLING_SERVICE))
+app.get("/api/me", protect, getCurrentUser)
+app.get("/", (req, res) => {
+    res.json({ message: "hello from gateway v5" })
 })
 
-const server = app.listen(port,()=>{
+const server = app.listen(port, () => {
     console.log(`gateway started at ${port}`)
 })
 server.timeout = 120000;
